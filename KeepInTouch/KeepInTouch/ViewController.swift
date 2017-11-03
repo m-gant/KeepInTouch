5deabf0360faecd7446ac48dc7532025646a901c
Mitchell Gant <mitchellgant@Mitchells-MacBook-Pro-5.local>
2017-11-03 19:54:51 -0400

    project creation

---
+++ KeepInTouch/KeepInTouch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeepInTouch
+//
+//  Created by Mitchell Gant on 11/3/17.
+//  Copyright © 2017 Mitchell Gant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // comment for GitHub
+    
+
+
+}
+
