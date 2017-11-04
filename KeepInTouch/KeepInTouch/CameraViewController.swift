cfa79a2d737e9eb4e4bb272f869b70e16ecff2b7
Mitchell Gant <mitchellgant@mitchells-macbook-pro-5.local>
2017-11-03 20:18:31 -0400

    user can now see camera input

--- KeepInTouch/KeepInTouch/CameraViewController.swift
+++ KeepInTouch/KeepInTouch/CameraViewController.swift
@@ -7,29 +7,126 @@
 //
 
 import UIKit
+import AVFoundation
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    
+    var captureSession = AVCaptureSession()
+    var previewLayer: CALayer!
+    var captureDevice: AVCaptureDevice!
+    var cameraPosition: AVCaptureDevice.Position!
+    var getFrame = false
+    var context = CIContext()
+    
+    
+    @IBOutlet weak var outputView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        //camera setup
+        cameraPosition = .back
+        prepareCamera()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func prepareCamera() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: cameraPosition).devices
+        captureDevice = availableDevices.first
+        do {
+            try captureDevice.lockForConfiguration()
+            captureDevice.activeVideoMaxFrameDuration = CMTimeMake(1, 15)
+            captureDevice.activeVideoMinFrameDuration = CMTimeMake(1, 15)
+            captureDevice.unlockForConfiguration()
+            
+        } catch {
+            print("error with setting up capture device")
+            print(error)
+        }
+        beginSession()
+        
     }
-    */
+    
+    
+    
+    func beginSession() {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(captureDeviceInput)
+        } catch {
+            print(error)
+        }
+        
+        let preview = AVCaptureVideoPreviewLayer(session: captureSession)
+        preview.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer = preview
+        outputView.layer.addSublayer(previewLayer)
+        previewLayer.frame = outputView.bounds
+        print(outputView.frame)
+        captureSession.startRunning()
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.videoSettings = [((kCVPixelBufferPixelFormatTypeKey as NSString) as String) :NSNumber(value:kCVPixelFormatType_32BGRA)]
+        dataOutput.alwaysDiscardsLateVideoFrames = true
+        
+        if captureSession.canAddOutput(dataOutput) {
+            captureSession.addOutput(dataOutput)
+        }
+        
+        captureSession.commitConfiguration()
+        
+        let picQueue = DispatchQueue(label: "com.mitchellgant.AniGif")
+        dataOutput.setSampleBufferDelegate(self, queue: picQueue)
+        
+        
+        
+        
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        if getFrame {
+            getFrame = false
+            if let image = self.captureFrame(buffer: sampleBuffer) {
+                processImage(image, completion: {
+                    //nothing for right now
+                })
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    
+    func processImage (_ image: UIImage, completion: () -> Void) {
+        print("processing image" )
+        
+        
+        
+    }
+    
+    func captureFrame(buffer: CMSampleBuffer) -> UIImage? {
+        if let pixBuffer = CMSampleBufferGetImageBuffer(buffer) {
+            let ciImage = CIImage(cvImageBuffer: pixBuffer)
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixBuffer), height: CVPixelBufferGetHeight(pixBuffer))
+            if let image = context.createCGImage(ciImage, from: imageRect) {
+                return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
+            }
+        }
+        return nil
+        
+    }
+    
+    
+    
+
+
 
 }
