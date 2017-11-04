a12f198f9769d003de6ca56b42ef702c47efa68a
Mitchell Gant <mitchellgant@mitchells-macbook-pro-5.local>
2017-11-03 23:33:35 -0400

    save pictures and email functionality added

--- KeepInTouch/KeepInTouch/CameraViewController.swift
+++ KeepInTouch/KeepInTouch/CameraViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
@@ -18,7 +19,8 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
     var cameraPosition: AVCaptureDevice.Position!
     var getFrame = false
     var context = CIContext()
-    
+    var requests = [VNRequest]()
+    var toEmail = ""
     
     @IBOutlet weak var outputView: UIView!
     
@@ -31,6 +33,14 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         cameraPosition = .back
         prepareCamera()
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? EmailViewController {
+            destination.toEmail = self.toEmail
+        } else {
+            super.prepare(for: segue, sender: self)
+        }
     }
     
     
@@ -107,6 +117,79 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
     
     func processImage (_ image: UIImage, completion: () -> Void) {
         print("processing image" )
+        let data = UIImagePNGRepresentation(image)
+        let microsoftURL = URL(string: "https://eastus.api.cognitive.microsoft.com/vision/v1.0/ocr?en&subscription-key=9caa6e915a534b339cc74be5e076aaaf")
+        var request = URLRequest(url: microsoftURL!)
+        request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = data!
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            print("fetching data")
+            guard error == nil else {
+                print("something messed up")
+                print(error?.localizedDescription)
+                return
+            }
+            
+            var returnedData = [String]()
+        
+            if let jsonData = try? JSONSerialization.jsonObject(with: data!, options: .mutableContainers) {
+                if let responseData = jsonData as? [String: Any] {
+                    let readableData = responseData["regions"] as! [[String: Any]]
+                    for dataChunk in readableData {
+                        if let lineSet = dataChunk["lines"] as? [[String: Any]] {
+                            for line in lineSet {
+                                var lineString = ""
+                                if let wordSet = line["words"] as? [[String: String]] {
+                                    for word in wordSet {
+                                        lineString += " "
+                                       lineString += word["text"]!
+                                    }
+                                }
+                               returnedData.append(lineString)
+                             }
+                        }
+                    }
+                } else {
+                    print("cannot cast readableData")
+                }
+                print(returnedData)
+                print("--------------")
+                print(self.getEmail(lineData: returnedData))
+                
+                if let email = self.getEmail(lineData: returnedData) {
+                    self.toEmail = email
+                    let alert = UIAlertController(title: "Next Steps", message: "Select an option below.", preferredStyle: .alert)
+                    let cancel = UIAlertAction(title: "Cancel", style: .default)
+                    let justSave = UIAlertAction(title: "Just Save Business Card", style: .default, handler: { (action) in
+                        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                        self.dismiss(animated: true, completion: {
+                            let alert2 = UIAlertController(title: "Image saved to camera roll.", message: nil, preferredStyle: .alert)
+                            alert2.addAction(UIAlertAction(title: "OK", style: .default))
+                            self.present(alert2, animated: true, completion: nil)
+                        })
+                        
+                    })
+                    
+                    let saveContinue = UIAlertAction(title: "Save and Continue to Email", style: .default, handler: { (action) in
+                        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                        self.performSegue(withIdentifier: "toEmailVC", sender: self)
+                    })
+                    
+                    alert.addAction(cancel)
+                    alert.addAction(justSave)
+                    alert.addAction(saveContinue)
+                    
+                    
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    //we didnt find an email
+                }
+                
+            }
+            
+        }
+        task.resume()
         
         
         
@@ -125,6 +208,34 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
     }
     
     
+    
+    
+    @IBAction func takePictureBtnPressed(_ sender: Any) {
+        
+        getFrame = true
+        
+    }
+    
+    func getEmail(lineData: [String]) -> String? {
+        var emailLine = ""
+        for line in lineData {
+            if line.contains("@") {
+                emailLine = line
+                break
+            }
+        }
+    
+        var words = emailLine.split(separator: " ")
+        var email: String?  = nil
+        for word in words {
+            if word.contains("@") {
+                email = String(word)
+                break
+            }
+        }
+        return email
+    
+    }
     
 
 
